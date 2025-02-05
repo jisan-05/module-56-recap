2baa8a07dddd690d1d89c75948f658f6511bcc88
Tanvir <zishantanvir05@gmail.com>
2025-02-05 22:43:34 +0600

    first commit

---
+++ simple-crud-client/src/App.jsx
@@ -0,0 +1,47 @@
+
+import "./App.css";
+
+function App() {
+
+  const handleAddUser = (e) => {
+    e.preventDefault()
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {name,email}
+    console.log(user)
+    fetch("http://localhost:5000/users", {
+      method:'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user)    
+    })
+
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      if(data.insertedId){
+        alert("User Added Successfully")
+        form.reset()
+      }
+    })
+  }
+
+    return (
+        <>
+            <div>
+                <h1>Simple Crud</h1>
+                <form onSubmit={handleAddUser}>
+                  <input type="text" name="name" />
+                  <br />
+                  <input type="email" name="email" id="" />
+                  <br />
+                  <input type="submit" value="Add User" />
+                </form>
+            </div>
+        </>
+    );
+}
+
+export default App;
---
+++ simple-crud-server/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// tanvirahmed1
+// Au5CMuB8frIj6QoM
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+    "mongodb+srv://tanvirahmed1:Au5CMuB8frIj6QoM@cluster0.pmlso.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const database = client.db("usersDB3");
+        const userCollection = database.collection("users");
+
+
+        app.post('/users', async (req,res) => {
+            const user = req.body;
+            console.log('new user', user)
+            const result = await userCollection.insertOne(user);
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!"
+        );
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close(); 
+    }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("SIMPLE CRUD IS RUNNING");
+});
+
+app.listen(port, () => {
+    console.log(`simple crud is running on port : ${port}`);
+});
